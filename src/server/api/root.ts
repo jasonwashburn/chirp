4b91af580aaf5b42dfc61efa2d32277ac7378056
Jason Washburn <jason.washburn@gmail.com>
2023-10-08 12:54:33 -0500

    next image tag, style up, 1:08:00

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { postRouter } from "./routers/posts";
+import { postsRouter } from "./routers/posts";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { postRouter } from "./routers/posts";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  posts: postRouter
+  posts: postsRouter
 });
 
 // export type definition of API
